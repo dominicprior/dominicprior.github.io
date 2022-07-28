ba2b2f9d2a5fdf04ae6d2ed63e6e37aeaac4d0b4
Dominic Prior <dominicprior42@gmail.com>
2022-07-28 09:45:43 +0100

    Update stars2

--- stars2.js
+++ stars2.js
@@ -13,17 +13,48 @@
 
 const winW  = window.innerWidth
 const winH = window.innerHeight
-
 let div = document.createElement('div')
 document.body.append(div)
 let draw = SVG().addTo('body').size(winW, winH)
 
-const numStars = 400
-const starRad = 0.01
+const numStars = 40
+const starRad = 1
+const boxSize = 100
+let furthestStars = 0
+let speed = 5
 
-const rnd = Math.random
+let eyeZ = 0
 
-let circles = [];
+function rnd(a, b) {
+  return a + (b - a) * Math.random
+}
+
+function rndColor() {
+  return new SVG.Color({ r: 150 + 100 * rnd(),
+                         g: 150 + 100 * rnd(),
+                         b: 150 + 100 * rnd() })
+}
+
+let stars = []
+
+function addNewStars() {
+  stars = stars.filter(star => star[2] < 0)
+  while (furthestStars < eyeZ + boxSize) {
+    const h = boxSize / 2
+    for (let i=0; i < numStars / 2; i++) {
+      const star = [rnd(-h, h), rnd(-h, h),
+                    rnd(furthestStars, furthestStars + h),
+                    rndColor()]
+      stars.push(star)
+    }
+    furthestStars += h
+  }
+  stars.sort((a, b) => hypotSq(a) - hypotSq(b))
+}
+
+function hypotSq(a) {
+  return a[0] * a[0] + a[1] * a[1] + a[2] * a[2]
+}
 
 for (let i=0; i < numStars; i++) {
   let circ = draw.circle(0).
@@ -36,28 +67,24 @@ for (let i=0; i < numStars; i++) {
   circles.push(circ)
 }
 
-const r = 50;
-
-let circ = draw.circle(200).center(200,100).fill('yellow').attr('foo', 6);
+let circ = draw.circle(200).center(200,100).fill('yellow').attr('foo', 6)
 
 function step(timestamp) {
-  if (timestamp < 5000) {
-    let t = timestamp / 1000;
-    let eyeZ = t - 1;
-    div.innerHTML = "t: " + t;
-    for (let c of circles) {
-      const z = eyeZ + c.attr('zz')
-      if (z < 0)
-        c.hide()
-      else {
-        c.show() 
-        c.radius(starRad / z * winW)
-        c.cx(c.attr('xx') / z * winW + winW / 2) 
-        c.cy(c.attr('yy') / z * winW + winH / 2) 
-      }
-    }
-    window.requestAnimationFrame(step);
+  let t = timestamp / 1000
+  if (t > 4) {
+    return
   }
+  div.innerHTML = "t: " + t
+  let eyeZ = speed * t
+  for (let circ of circles) {
+    const z = circ[2] - eyeZ
+    if (z > 0) {
+      draw.circle(starDiam / z).center(
+        circ[0] / z * winW + winW / 2,
+        circ[1] / z * winW + winH / 2).fill(circ[3])
+    }
+  }
+  window.requestAnimationFrame(step);
 }
 
 window.requestAnimationFrame(step);
