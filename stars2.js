2350ebd71632c4c1a7ef4335502789e1fbfd2592
Dominic Prior <dominicprior42@gmail.com>
2022-07-28 10:57:47 +0100

    Working stars 2

--- stars2.js
+++ stars2.js
@@ -17,29 +17,31 @@ let div = document.createElement('div')
 document.body.append(div)
 let draw = SVG().addTo('body').size(winW, winH)
 
-const numStars = 40
-const starRad = 1
+const numStars = 640
+const starDiam = 2
 const boxSize = 100
 let furthestStars = 0
-let speed = 5
+let speed = 25
 
 let eyeZ = 0
 
 function rnd(a, b) {
-  return a + (b - a) * Math.random
+  return a + (b - a) * Math.random()
 }
 
 function rndColor() {
-  return new SVG.Color({ r: 150 + 100 * rnd(),
-                         g: 150 + 100 * rnd(),
-                         b: 150 + 100 * rnd() })
+  return new SVG.Color({ r: rnd(150, 255),
+                         g: rnd(150, 255),
+                         b: rnd(150, 255) })
 }
 
 let stars = []
 
 function addNewStars() {
-  stars = stars.filter(star => star[2] < 0)
+  console.log('addNewStars:  ' + furthestStars + '  ' + eyeZ + '  ' + boxSize)
+  stars = stars.filter(star => star[2] > 0)
   while (furthestStars < eyeZ + boxSize) {
+    console.log('adding')
     const h = boxSize / 2
     for (let i=0; i < numStars / 2; i++) {
       const star = [rnd(-h, h), rnd(-h, h),
@@ -49,42 +51,33 @@ function addNewStars() {
     }
     furthestStars += h
   }
-  stars.sort((a, b) => hypotSq(a) - hypotSq(b))
+  stars.sort((a, b) => hypotSq(b) - hypotSq(a))
 }
 
 function hypotSq(a) {
   return a[0] * a[0] + a[1] * a[1] + a[2] * a[2]
 }
 
-for (let i=0; i < numStars; i++) {
-  let circ = draw.circle(0).
-  attr('xx', 2*rnd()-1).
-  attr('yy', 2*rnd()-1).
-  attr('zz', 2*rnd()-1).
-  fill(new SVG.Color({ r: 150 + 100 * rnd(),
-                       g: 150 + 100 * rnd(),
-                       b: 150 + 100 * rnd() }))
-  circles.push(circ)
-}
-
-let circ = draw.circle(200).center(200,100).fill('yellow').attr('foo', 6)
+draw.circle(200).center(200,100).fill('yellow').attr('foo', 6)
 
 function step(timestamp) {
   let t = timestamp / 1000
-  if (t > 4) {
+  div.innerHTML = "t: " + t
+  if (t > 15) {
     return
   }
-  div.innerHTML = "t: " + t
-  let eyeZ = speed * t
-  for (let circ of circles) {
-    const z = circ[2] - eyeZ
-    if (z > 0) {
-      draw.circle(starDiam / z).center(
-        circ[0] / z * winW + winW / 2,
-        circ[1] / z * winW + winH / 2).fill(circ[3])
+  eyeZ = speed * t
+  addNewStars()
+  draw.clear()
+  for (let star of stars) {
+    const z = star[2] - eyeZ
+    if (z > 0 && z < boxSize) {
+      draw.circle(starDiam * boxSize / z).center(
+        star[0] / z * winW + winW / 2,
+        star[1] / z * winW + winH / 2).fill(star[3])
     }
   }
-  window.requestAnimationFrame(step);
+  window.requestAnimationFrame(step)
 }
 
-window.requestAnimationFrame(step);
+window.requestAnimationFrame(step)
