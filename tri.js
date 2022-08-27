bc8217b2ee803d4324f30ed5c6c5b460b05a92dc
Dominic Prior <dominicprior42@gmail.com>
2022-08-27 09:52:53 +0100

    Add another attribute to tri.js

--- tri.js
+++ tri.js
@@ -1,12 +1,15 @@
 'use strict'
 
 let gl = document.querySelector('canvas').getContext('webgl')
+gl.clearColor(0.0, 0.0, 0.3, 1.0)  // Clear to black, fully opaque
+gl.clear(gl.COLOR_BUFFER_BIT)
 
 const vs = `
-attribute vec2 position;
+attribute vec4 position;
+attribute vec4 offset;
 uniform float scale;
 void main() {
-  gl_Position = vec4(scale * position, 0, 1);
+  gl_Position = position + offset;
 }`
 
 const fs = `
@@ -17,7 +20,8 @@ void main() {
 
 const programInfo = twgl.createProgramInfo(gl, [vs, fs])
 const arrays = {
-    position: [ 0, 0, 0,1,   0, 0.5, 0,1,   0.7, 0, 0,1,  ],
+    position: { numComponents: 4, data: [0, 0, 0, 1,   0, 0.9, 0, 1,   0.9, 0, 0, 1,], },
+    offset: { numComponents: 4, data:  [ 0.2, 0, 0,0,   0,0    ,0,0,   0,0,    0,0,   ], },
   }
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 
