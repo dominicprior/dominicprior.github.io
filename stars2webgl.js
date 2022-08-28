217acd8bd0662cbaf5ac69144810073f2544bfaa
Dominic Prior <dominicprior42@gmail.com>
2022-08-28 10:48:04 +0100

    Add animation to star2webgl

--- stars2webgl.js
+++ stars2webgl.js
@@ -7,7 +7,6 @@
 const numStars = 240
 const starDiam = 15
 const boxSize = 1000
-let furthestStars = 0
 let speed = 15
 let eyeZ = 0
 
@@ -16,9 +15,11 @@ gl.clearColor(0.0, 0.0, 0.3, 1.0)
 gl.clear(gl.COLOR_BUFFER_BIT)
 
 const vs = `
-attribute vec4 position;
+attribute vec4 pos;
+uniform float eyeZ;
 void main() {
-  gl_Position = position;
+  float z = pos.z - eyeZ;
+  gl_Position = vec4(pos.x / z, pos.y / z, 0, 1);
 }`
 
 const fs = `
@@ -35,27 +36,36 @@ let stars = []
 addNewStars()
 
 let arrays = {
-  position: { numComponents: 4, data: stars, },
+  pos: { numComponents: 4, data: stars, },
 }
 let bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
 
 
-function step(timestamp) {
-  let t = timestamp / 1000
-  if (t > 6) {
-    return
-  }
+function step(t) {
+  t /= 1000
+  if (t > 1) { return }
   eyeZ = speed * t
   let uniforms = { eyeZ: eyeZ, }
   twgl.setUniforms(programInfo, uniforms)
   twgl.drawBufferInfo(gl, bufferInfo)
-  // window.requestAnimationFrame(step)
+  window.requestAnimationFrame(step)
 }
 
 window.requestAnimationFrame(step)
 
 
+function addNewStars() {
+  stars = [ 0, 0, 2, 1,   0, 0.9, 2, 1,   0.9, 0, 2, 1,]
+  return
+  const h = boxSize / 2
+  for (let i=0; i < numStars; i++) {
+    stars.push(rnd(-h, h),
+               rnd(-h, h),
+               rnd(0, 2 * h),
+               1)
+  }
+}
 
 function rnd(a, b) {
   return a + (b - a) * Math.random()
@@ -65,19 +75,4 @@ function rndColor() {
   return new SVG.Color({ r: rnd(100, 255),
                          g: rnd(100, 255),
                          b: rnd(100, 255) })
-}
-
-function addNewStars() {
-  stars = [ 0, 0, 0, 1,   0, 0.9, 0, 1,   0.9, 0, 0, 1,]
-  return
-  while (furthestStars < eyeZ + boxSize) {
-    const h = boxSize / 2
-    for (let i=0; i < numStars / 2; i++) {
-      stars.push(rnd(-h, h),
-                 rnd(-h, h),
-                 rnd(furthestStars, furthestStars + h),
-                 1)
-    }
-    furthestStars += h
-  }
 }
