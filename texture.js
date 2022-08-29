9de053c1406c563f905287e773f258223a1eecf3
Dominic Prior <dominicprior42@gmail.com>
2022-08-29 20:26:30 +0100

    Tidy texture.js

--- texture.js
+++ texture.js
@@ -3,14 +3,10 @@
 
 const vs = `#version 300 es
 in vec2 pos;
-in vec3 colour;
-in vec2 uv;
-// out vec3 fragColour;
 in vec2 foo;
 out vec2 vTexCoord;
 uniform float scale;
 void main() {
-  // fragColour = colour;
   gl_PointSize = 70.0;
   gl_Position = vec4(pos * scale, 0.0, 1.0);
   vTexCoord = foo;
@@ -18,12 +14,10 @@ void main() {
 
 const fs = `#version 300 es
 precision mediump float;
-// in vec3 fragColour;
 in vec2 vTexCoord;
 out vec4 finalColour;
 uniform sampler2D uSampler;
 void main() {
-  // finalColour = vec4(fragColour, 1);
   finalColour = texture(uSampler, vTexCoord);
 }`;
 
@@ -36,8 +30,6 @@ gl.useProgram(programInfo.program)
 
 let arrays = {
   pos:    { numComponents: 2, data:  [0, 0,      0.5, 0.5,  0, 0.9,   1.6, 0 ], },
-  colour: { numComponents: 3, data:  [0, 0,1,    0, 1, 0,   1,0,0,    1,0,0  ], },
-  uv:     { numComponents: 2, data:  [0, 0,      1, 0,      0, 1,     1, 1, ], },
   indices: [0,1,2, 0,1,3]
 }
 let bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
