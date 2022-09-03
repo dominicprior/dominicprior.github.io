048250c073bb844c52f03caf28cb54ab3c1f3fee
Dominic Prior <dominicprior42@gmail.com>
2022-09-03 16:10:55 +0100

    Update mat.js

--- mat.js
+++ mat.js
@@ -26,8 +26,23 @@ gl.useProgram(programInfo.program)
 const view = mat4.create()
 const proj = mat4.create()
 
+// negates x and z.  i.e. turns thru 180 about the y-axis
+// -1, 0, 0, 0,   0, 1, 0, 0,   0, 0, -1, 0,   0, 0, 0, 1   diagonal matrix
+mat4.lookAt(view, [0,0,0], // eye
+        [0,0,1],   // looking at
+        [0,1,0]);
+
+// 1, 0, 0, 0    column major!  i.e. this is the first column
+// 0, 1, 0, 0
+// 0, 0, -1, -1      z is flipped (to cancel out the view z-flip)
+// 0, 0, -0.4, 0      the -0.4 is a z translation
+mat4.perspective(proj, Math.PI / 2, 1,   // flips z
+        .2,   //near
+        null    //far
+        );
+
 let arrays = {
-  pos: { numComponents: 3, data:  [ 0, 0, 0.5,  0.3, 0, 0.5 ], },
+  pos: { numComponents: 3, data:  [ 0, 0, 0.5,   0.5, 0, 0.5,   0.5, 0, 0.6, ], },
 }
 
 const uniforms = {view: view, proj: proj}
