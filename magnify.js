d1c3c723c09a87728887fc4b25ae79c2e6054673
Dominic Prior <dominicprior42@gmail.com>
2022-09-06 09:00:42 +0100

    Change magnify.js to pixel coords

--- magnify.js
+++ magnify.js
@@ -1,27 +1,35 @@
 // Draws a red disc using the fragment shader.
 
 const vs = `#version 300 es
-// uniform vec2 canvasSize;
+uniform float canvasSize;   // assuming square for now
 in float rad;  // in clip space
 in vec2 centre;   // in clip space
-in vec2 pos;  // in disc space (e.g. interpolation of things like (1,1) and (-1,1))
+in vec2 coord;  // in *disc* space (e.g. interpolation of things like (1,1) and (-1,1))
 in vec3 col;
-out vec2 fsPos;
+out float r;
+out vec2 pos;
+out vec2 c;   // centre, in canvas pixel coords
 out vec3 fsCol;
 void main() {
-  fsPos = pos;
+  r = rad * canvasSize * 0.5;
+  vec2 fragPos = centre + rad * coord;   // in clip space
+  pos = (fragPos + 1.0) * canvasSize * 0.5;  // in canvas pixel coords
+  c = (centre + 1.0) * canvasSize * 0.5;  // in canvas pixel coords
   fsCol = col;
-  gl_Position = vec4(centre + rad * pos, 0, 1);
+  gl_Position = vec4(fragPos, 0, 1);   // in clip space
 }`;
 const fs = `#version 300 es
 precision mediump float;
 //uniform vec2 canvasSize;
-in vec2 fsPos;
+in float r;   // in canvas pixel coords
+in vec2 pos;  // in canvas pixel coords
+in vec2 c;    // centre, in canvas pixel coords
 in vec3 fsCol;
 out vec4 finalCol;
 void main() {
-  if (length(fsPos) > 1.0)
+  if (length(pos - c) > r) {
     discard;
+  }
   finalCol = vec4(fsCol, 1);
 }`;
 let canvas = document.querySelector('canvas')
@@ -38,13 +46,13 @@ const arrays = {
   centre: { size: 2, data: [ 0,0, ], divisor: 1},
   rad:    { size: 1, data: [ 0.2, ], divisor: 1},
   col:    { size: 3, data: [ 1,0,0, ], divisor: 1},
-  pos:    { size: 2, data: [ -1,-1, 1,-1, 1,1, -1,-1, 1,1, -1,1, ] },
+  coord:  { size: 2, data: [ -1,-1, 1,-1, 1,1, -1,-1, 1,1, -1,1, ] },
 }
 let bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
 
 const uniforms = {
-  canvasSize: [ canvas.width, canvas.height ],
+  canvasSize: canvas.width,
 }
 twgl.setUniforms(programInfo, uniforms)
 
