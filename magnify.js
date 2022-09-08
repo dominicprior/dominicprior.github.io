7aa37ddf04410fad38fb1d22112028206d7995af
Dominic Prior <dominicprior42@gmail.com>
2022-09-08 08:39:20 +0100

    Remove commented out code - the program still works!

--- magnify.js
+++ magnify.js
@@ -16,35 +16,16 @@ out vec4 finalCol;
 void main() {
     finalCol = vec4(0,1,1, 1);
 }`;
-pi = twgl.createProgramInfo(gl, [vs, fs])
-gl.useProgram(pi.program)
-/*
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER,
-      new Float32Array([ -1,-1, 1,-1, 1,1, ]), gl.STATIC_DRAW);
-loc = gl.getAttribLocation(pi.program, 'c')
-gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0)
-gl.enableVertexAttribArray(loc)
-
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER,
-      new Float32Array([ 0.5 ]), gl.STATIC_DRAW);
-loc = gl.getAttribLocation(pi.program, 'r')
-gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0)
-gl.enableVertexAttribArray(loc)
-gl.vertexAttribDivisor(loc, 1)
-
-gl.drawArraysInstanced(gl.TRIANGLES, 0, 3, 1)  // mode, first, count, instanceCount
-*/
+programInfo = twgl.createProgramInfo(gl, [vs, fs])
+gl.useProgram(programInfo.program)
 
 arrays = {
   r:  { size: 1, data: [ 0.5, ], divisor: 1},
   c:  { size: 2, data: [ -1,-1, 1,-1, 1,1, ] },
 }
 bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
-twgl.setBuffersAndAttributes(gl, pi, bufferInfo)
+twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
 twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES, 3, 0, 1)
-
 //------
 vs = `#version 300 es
 in vec2 pos;
@@ -59,15 +40,6 @@ void main() {
 }`
 pi = twgl.createProgramInfo(gl, [vs, fs])
 gl.useProgram(pi.program)
-/*
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER,
-      new Float32Array([ 0, 0, 0, -1, -1, 0, ]), gl.STATIC_DRAW);
-loc = gl.getAttribLocation(pi.program, 'pos')
-gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(loc);
-gl.drawArrays(gl.TRIANGLES, 0, 3)
-*/
 arrays = { pos: { size: 2, data: [ 0, 0,   0, -1,   -1, 0,], },  }
 bi = twgl.createBufferInfoFromArrays(gl, arrays)
 twgl.setBuffersAndAttributes(gl, pi, bi)
