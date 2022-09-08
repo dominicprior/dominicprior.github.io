753a4e88a555f0b9fe169b1f06b58ba1147dd202
Dominic Prior <dominicprior42@gmail.com>
2022-09-08 16:10:15 +0100

    Shorten magnify.js

--- magnify.js
+++ magnify.js
@@ -1,14 +1,12 @@
-let canvas = document.querySelector('canvas')
-let gl = canvas.getContext('webgl2')
+gl = document.querySelector('canvas').getContext('webgl2')
 gl.clearColor(0,0,0.4, 1.0)
 gl.clear(gl.COLOR_BUFFER_BIT)
 //------
 vs = `#version 300 es
-in vec2 c;
 in float r;
+in vec2 c;
 void main() {
-  //gl_Position = vec4(c, 0, 1);           // This line allows the downstream program to run.
-  gl_Position = vec4(r * c, 0, 1);  // This line lines doesn't.
+  gl_Position = vec4(r * c, 0, 1);
 }`;
 fs = `#version 300 es
 precision mediump float;
@@ -16,16 +14,15 @@ out vec4 finalCol;
 void main() {
     finalCol = vec4(0,1,1, 1);
 }`;
-programInfo = twgl.createProgramInfo(gl, [vs, fs])
-gl.useProgram(programInfo.program)
-
+p = twgl.createProgramInfo(gl, [vs, fs])
+gl.useProgram(p.program)
 arrays = {
-  r:  { size: 1, data: [ 0.5, ], divisor: 1},
-  c:  { size: 2, data: [ -1,-1, 1,-1, 1,1, ] },
+  r:  { size: 1, data: [ 0.5 ], divisor: 1 },
+  c:  { size: 2, data: [ -1,-1, 1,-1, 1,1, ] }
 }
-bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
-twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
-twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES, 3, 0, 1)
+b = twgl.createBufferInfoFromArrays(gl, arrays)
+twgl.setBuffersAndAttributes(gl, p, b)
+twgl.drawBufferInfo(gl, b, gl.TRIANGLES, 3, 0, 1)
 //------
 vs = `#version 300 es
 in vec2 pos;
@@ -38,9 +35,13 @@ out vec4 finalCol;
 void main() {
   finalCol = vec4(1, 0.5, 0.5, 1);
 }`
-pi = twgl.createProgramInfo(gl, [vs, fs])
-gl.useProgram(pi.program)
-arrays = { pos: { size: 2, data: [ 0, 0,   0, -1,   -1, 0,], },  }
-bi = twgl.createBufferInfoFromArrays(gl, arrays)
-twgl.setBuffersAndAttributes(gl, pi, bi)
-twgl.drawBufferInfo(gl, bi)
+p = twgl.createProgramInfo(gl, [vs, fs])
+gl.useProgram(p.program)
+arrays = {
+  pos: { size: 2, data: [ 0, 0, 0, -1, -1, 0 ],
+    // divisor: 0
+  }
+}
+b = twgl.createBufferInfoFromArrays(gl, arrays)
+twgl.setBuffersAndAttributes(gl, p, b)
+twgl.drawBufferInfo(gl, b)
