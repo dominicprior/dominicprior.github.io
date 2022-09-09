7f45ae7a91b719e2ea1017510f4a2191216528e2
Dominic Prior <dominicprior42@gmail.com>
2022-09-09 11:13:45 +0100

    Add some comments and tidy up

--- magnify.js
+++ magnify.js
@@ -1,9 +1,42 @@
-// Draws a red disc using the fragment shader.
+'use strict'
+
+// Draws two anti-aliased discs along with magnified versions
+// of them.
+
+// The anti-aliasing calculates the proportion of each pixel
+// covered by the disk and sets the alpha value accordingly.
+// In other words, it doesn't do super-sampling.
+
+// Nor does it use the more sophisticated anti-aliasing methods
+// in https://en.wikipedia.org/wiki/Spatial_anti-aliasing ,
+// which may explain why the discs look slightly square shaped.
+
+// However, this simple approach should remove any jagged edges
+// and any sudden jumps when the discs move by a fraction of a pixel.
+
+// For gamma correction, the alpha values are simply
+// raised to the power of 1/2.2 (which is approximately 0.4545),
+// which may not be correct.  See:
+//   http://www.realtimerendering.com/blog/gpus-prefer-premultiplication/
+//   https://limnu.com/webgl-blending-youre-probably-wrong/
+//   http://renderwonk.com/blog/index.php/archive/adventures-with-gamma-correct-rendering/
+
+let canvas = document.querySelector('canvas')
+let gl = canvas.getContext('webgl2',
+      { //alpha: false,
+        //premultipliedAlpha: true,
+       })
+gl.clearColor(0,0,0, 1.0)
+gl.clear(gl.COLOR_BUFFER_BIT)
+gl.enable(gl.BLEND)
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+
+// ------ Draw the discs ------
 
 let vs = `#version 300 es
 uniform float canvasSize;   // assuming square for now
-in float rad;  // in clip space
-in vec2 centre;   // in clip space
+in float rad;   // in clip space
+in vec2 centre; // in clip space
 in vec2 coord;  // in *disc* space (e.g. interpolation of things like (1,1) and (-1,1))
 in vec3 col;
 out float r;
@@ -14,13 +47,12 @@ void main() {
   r = rad * canvasSize * 0.5;
   vec2 fragPos = centre + rad * coord;   // in clip space
   pos = (fragPos + 1.0) * canvasSize * 0.5;  // in canvas pixel coords
-  c = (centre + 1.0) * canvasSize * 0.5;  // in canvas pixel coords
+  c   = (centre  + 1.0) * canvasSize * 0.5;  // in canvas pixel coords
   fsCol = col;
-  gl_Position = vec4(fragPos, 0, 1);   // in clip space
+  gl_Position = vec4(fragPos, 0, 1);     // in clip space
 }`;
 let fs = `#version 300 es
 precision mediump float;
-//uniform vec2 canvasSize;
 in float r;   // in canvas pixel coords
 in vec2 pos;  // in canvas pixel coords
 in vec2 c;    // centre, in canvas pixel coords
@@ -31,10 +63,16 @@ float f(float a, float b, float c) {
   return c == a ? 0.0 : (b - a) / (c - a);
 }
 
+// The main function sets the alpha value according to how much
+// of the current pixel is covered by the disc.
+
+// The are calculations are approximations that avoid doing any
+// square roots.
+
 void main() {
-  vec2 d = abs(pos - c);
+  vec2 d = abs(pos - c);  // to reduce the cases to one quadrant
   if (d.y > d.x)
-    d = d.yx;
+    d = d.yx;             // to reduce down to one octant
   vec2 near = d - 0.5;
   vec2 far = d + 0.5;
   float nearSq = dot(near, near);
@@ -48,35 +86,32 @@ void main() {
     finalCol = vec4(fsCol, 1);  // completely inside
     return;
   }
+
+  // The remaining cases depend on which corners of the pixel
+  // are inside the disc.
+
   vec2 topLeft = vec2(near.x, far.y);
   float topLeftSq = dot(topLeft, topLeft);
   vec2 botRight = vec2(near.y, far.x);
   float botRightSq = dot(botRight, botRight);
   float k = f(nearSq, radSq, botRightSq);
   if (radSq < topLeftSq) {
+    // then the intersection is a triangle, where k is the width and h is the height.
     float h = f(nearSq, radSq, topLeftSq);
     finalCol = vec4(fsCol, pow((h * k * 0.5), 0.4545));
     return;
   }
   float k2 = f(topLeftSq, radSq, farSq);
   if (radSq < botRightSq) {
+    // then the intersection is a trapezium.
     finalCol = vec4(fsCol, pow(((k + k2) * 0.5), 0.4545));
     return;
   }
+  // else the intersection is the whole pixel minus a triangle.
   float h2 = f(botRightSq, radSq, farSq);
   float opacity = 1.0 - (1.0 - h2) * (1.0 - k2) * 0.5;
   finalCol = vec4(fsCol, pow(opacity, 0.4545));
 }`;
-let canvas = document.querySelector('canvas')
-let gl = canvas.getContext('webgl2',
-      { //alpha: false,
-        //premultipliedAlpha: true,
-       })
-//gl.clearColor(0.8, 0.9, 1.0, 1.0)
-gl.clearColor(0,0,0, 1.0)
-gl.clear(gl.COLOR_BUFFER_BIT)
-gl.enable(gl.BLEND)
-gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
 let programInfo = twgl.createProgramInfo(gl, [vs, fs])
 gl.useProgram(programInfo.program)
 
@@ -88,18 +123,18 @@ let arrays = {
 }
 let bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
-
 const uniforms = {
   canvasSize: canvas.width,
 }
 twgl.setUniforms(programInfo, uniforms)
-
 twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES, 6, 0, 2)
 
-// ---------------
+// ------ Draw the magnified view ------
 
-let pixels = new Uint8Array(1600)
-gl.readPixels(0, 380, 20, 20, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+const width = 20
+const height = 20
+let pixels = new Uint8Array(4 * width * height)
+gl.readPixels(0, canvas.height - height, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
 
 vs = `#version 300 es
 in vec2 pos;
@@ -121,9 +156,13 @@ void main() {
 programInfo = twgl.createProgramInfo(gl, [vs, fs])
 gl.useProgram(programInfo.program)
 
+const left = -0.7
+const bot = -0.7
+const right = 0.7
+const topp = 0.7
 arrays = {
-  pos:      { size: 2, data: [ -0.7,-0.7, 0.7,-0.7, 0.7,0.7, -0.7,-0.7, 0.7,0.7, -0.7,0.7, ] },
-  texcoord: { size: 2, data: [ 0,0, 1,0, 1,1, 0,0, 1,1, 0,1, ] },
+  pos:      { size: 2, data: [ left,bot, right,bot, right,topp, left,bot, right,topp, left,topp, ], divisor: 0 },
+  texcoord: { size: 2, data: [ 0,0, 1,0, 1,1, 0,0, 1,1, 0,1, ], divisor: 0 },
 }
 bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
