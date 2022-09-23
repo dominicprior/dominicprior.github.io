d183db5046f312655f92ac46ca5575f29e44b68f
Dominic Prior <dominicprior42@gmail.com>
2022-09-23 22:01:01 +0100

    Add the prHist function to utils.js

--- webgl/utils.js
+++ webgl/utils.js
@@ -8,3 +8,27 @@ function prState() {
     pr(params.map(p => gl.getVertexAttrib(i, gl[p])))
   }
 }
+
+function prHist(pixels) {
+  gl.flush()
+  let colors = {}
+  let para = document.createElement('p')
+  document.body.appendChild(para)
+  for (let i=0; i < pixels.length; i +=4) {
+    let color = ''
+    for (let j=0; j < 3; j++) {
+      color += pixels[i+j] + ', '
+    }
+    if (colors[color]) {
+      colors[color]++
+    }
+    else {
+      colors[color] = 1
+    }
+  }
+  for (let c in colors) {
+    let msg = c + ' --- ' + colors[c]
+    pr(msg)
+    para.insertAdjacentHTML('beforeend', msg + '<br>')
+  }
+}
