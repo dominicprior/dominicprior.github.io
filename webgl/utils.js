26bdeb394921dfc7f00a6b46da1843c3be21a2e5
Dominic Prior <dominicprior42@gmail.com>
2022-09-25 11:44:45 +0100

    Add the magnify function

--- webgl/utils.js
+++ webgl/utils.js
@@ -32,3 +32,56 @@ function prHist(pixels) {
     para.insertAdjacentHTML('beforeend', msg + '<br>')
   }
 }
+
+function magnify(
+  gl,
+  scale,
+  x, y, clipW, clipH,   // src in clip coords
+  u, v,                 // dest in clip coords
+) {
+
+  x = Math.round((x + 1) * gl.canvas.width * 0.5)
+  y = Math.round((y + 1) * gl.canvas.height * 0.5)
+  let w = Math.round(clipW * gl.canvas.width * 0.5)
+  let h = Math.round(clipH * gl.canvas.width * 0.5)
+
+  let pixels = new Uint8Array(4 * w * h)
+  gl.readPixels(x, y, w, h, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+
+  let vs = `#version 300 es
+  in vec2 pos;
+  in vec2 texcoord;
+  out vec2 v_texCoord;
+  void main() {
+    v_texCoord = texcoord;
+    gl_Position = vec4(pos, 0.0, 1.0);
+  }`;
+  let fs = `#version 300 es
+  precision mediump float;
+  in vec2 v_texCoord;
+  out vec4 finalColour;
+  uniform sampler2D s;
+  void main() {
+    finalColour = texture(s, v_texCoord);
+  }`;
+  
+  let pi = twgl.createProgramInfo(gl, [vs, fs])
+  gl.useProgram(pi.program)
+  
+  const right = u + clipW * scale
+  const topp  = v + clipH * scale
+  let arrays = {
+    pos:      { size: 2, data: [ u,v, right,v, right,topp, u,v, right,topp, u,topp, ], divisor: 0 },
+    texcoord: { size: 2, data: [ 0,0, 1,0, 1,1, 0,0, 1,1, 0,1, ], divisor: 0 },
+  }
+  let bi = twgl.createBufferInfoFromArrays(gl, arrays)
+  twgl.setBuffersAndAttributes(gl, pi, bi)
+  
+  twgl.createTexture(gl, {
+    mag: gl.NEAREST,
+    min: gl.LINEAR,
+    src: pixels,
+    width: w
+  })
+  twgl.drawBufferInfo(gl, bi)
+}
